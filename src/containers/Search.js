57584a04203879105dc0082c58939a17d2939e2f
thenewsupercell <cuberlouis@gmail.com>
2021-03-28 12:26:17 -0400

    got button working

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -10,12 +10,12 @@ const Search = () => {
                 <div class='row'>
                     <div class='col-lg-8 col-md-10 col-sm-11 mx-auto'>
                         <form class='order-1 d-flex flex-sm-row flex-column align-items-center' action="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
-                        <input type="text" class='border-0 form-control h-100 mt-2 mt-sm-0' defaultValue={url} onChange={e => setUrl(e.target.value)}/>
+                        <textarea type="text" class='border-0 form-control h-100 mt-2 mt-sm-0' defaultValue={url} onChange={e => setUrl(e.target.value)} rows="3"/>
                         </form>
                     </div>
                 </div>
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noreferrer noopener"><button class='btn btn-primary mr-sm-1 mt-4 mt-sm-0 flex-fill'>Search</button></a>
-                <h1>w{url}</h1>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noreferrer noopener"><button class='btn btn-primary mr-sm-1 mt-4 mt-sm-0 flex-fill'>Press this button</button></a>
+                <h1>{url}</h1>
             </div>
         </>
     )
